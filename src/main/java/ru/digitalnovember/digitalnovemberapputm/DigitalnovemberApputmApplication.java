39ec5f68d1fb5a30523743393f675dfcc4c6aa54
17750859 <Sopilov.N.V@sberbank.ru>
2019-11-29 22:46:18 +0300

    initial

---
+++ src/main/java/ru/digitalnovember/digitalnovemberapputm/DigitalnovemberApputmApplication.java
@@ -0,0 +1,13 @@
+package ru.digitalnovember.digitalnovemberapputm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalnovemberApputmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalnovemberApputmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/digitalnovember/digitalnovemberapputm/DigitalnovemberApputmApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.digitalnovember.digitalnovemberapputm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalnovemberApputmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
