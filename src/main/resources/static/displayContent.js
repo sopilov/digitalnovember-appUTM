54018ee39b0687b35a7d241a2c7a7aa57057fe9c
Dmitry Tsygankov <dmitry.u.ts@gmail.com>
2019-12-01 23:45:56 +0300

    display on click

---
+++ src/main/resources/static/displayContent.js
@@ -0,0 +1,5 @@
+const recountBtn = document.getElementById('recountBtn');
+const mainContent = document.getElementById('mainContent');
+recountBtn.onclick = function () {
+    mainContent.style.display = 'block';
+}
