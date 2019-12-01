6a24e469456f88600a2232b1cb4a76f53d8b894b
Dmitry Tsygankov <dmitry.u.ts@gmail.com>
2019-12-01 22:24:19 +0300

    companies added

---
+++ src/main/resources/static/companies.js
@@ -0,0 +1,28 @@
+async function getCompanies() {
+    const companies = await fetch('api/companies/', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            }
+        }
+    );
+    return companies.json();
+}
+
+getCompanies()
+.then(response => {
+    console.log(response);
+    updateCompanies(response);
+});
+
+function updateCompanies(companies) {
+    const selectBar = document.getElementById('_safe_id_21l2qfaeaae');
+    selectBar.innerHTML = '';
+
+    companies.forEach(company => {
+        const option = document.createElement('option');
+        option.value = company.id;
+        option.innerHTML = company.name;
+        selectBar.appendChild(option);
+    });
+}
